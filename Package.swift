0ab8656c164f91421f071d2086c209356d0e9922
Jordan Kay <jordan@fleuronic.com>
2023-05-17 11:01:46 -0400

    Remove Layoutless dependency

--- Package.swift
+++ Package.swift
@@ -16,14 +16,12 @@ let package = Package(
 		.package(url: "https://github.com/Fleuronic/Ergo", branch: "main"),
 		.package(url: "https://github.com/ReactiveCocoa/ReactiveSwift.git", branch: "swift-concurrency"),
 		.package(url: "https://github.com/ReactiveCocoa/ReactiveCocoa", from: "12.0.0"),
-        .package(url: "https://github.com/DeclarativeHub/Layoutless", .upToNextMajor(from: "0.4.0"))
     ],
     targets: [
 		.target(
 			name: "ErgoUIKit",
 			dependencies: [
 				"Ergo",
-				"Layoutless",
 				"ReactiveSwift",
 				"ReactiveCocoa"
 			]
